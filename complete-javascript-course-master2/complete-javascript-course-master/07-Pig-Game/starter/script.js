a9b7fbed23aa5ca7ad96f7289c9bf49956b2b3fd
28691281 <juliediem@gmail.com>
2021-12-04 18:31:35 -0500

    Updated work

--- complete-javascript-course-master2/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master2/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,175 +1,118 @@
 'use strict';
 
-//First player to reach 100 points wins the game
-//Everytime you roll a one, you lose all of your points
-//You have an option to reset the game
+//Put Scores to 0 and remove dice when initializing
 
-//Players can hold, or roll dice
-//When players hold, this switches to the other player; the other player fades into the background when they are not playing
+//The following two lines do the same thing
+//El--> Element
+//We are defining all the elements once first so that we don't have to call them each time later
+const player0El=document.querySelector('.player--0');
+const player1El=document.querySelector('.player--1');
+const score0El=document.querySelector('#score--0');
+const score1El=document.getElementById('score--1');
+const current0El=document.getElementById('current--0');
+const current1El=document.getElementById('current--1');
+const diceEl=document.querySelector('.dice');
+const btnNew=document.querySelector('.btn--new');
+const btnRoll=document.querySelector('.btn--roll');
+const btnHold=document.querySelector('.btn--hold');
 
 
-//Dice rolls random number each time, and adds score 
+let playing = true;
+//Scores are stored in array
+let scores = [0, 0];
+let currentScore = 0;
+//First player is 0, and second one is 1
+let activePlayer = 0;
+
+//Starting Conditions
+const startConditions = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  playing=true;
+  scores=[0,0];
+  currentScore=0;
+  current0El.textContent=0;
+  current1El.textContent=0;
+  activePlayer=0;
+};
+
+startConditions();
 
 
-//Initialize variable
-let currDiceRoll=5;
 
-//Generate Random Dice Roll
-function rollDice() {
-    return Math.trunc(Math.random()*6)+1;
+//Function for switching players
+
+const switchPlayer=function() {
+    //Switch to next player
+    //Reset Current Score back to 0
+    document.getElementById(`current--${activePlayer}`).textContent=0;
+    //Reset the current score to 0 in a local variable
+    currentScore=0;
+    //Switch to other player
+    activePlayer=activePlayer===0?1:0;
+    //Toggle add class if it's not there, and remove class if it is there
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
 };
 
 
-
-
-
-//Display Dice Roll Image
-const diceImg=function(img) {
-    document.querySelector('.dice').src=img;
-};
-
-let updateDiceImg=function() {
-    switch(currDiceRoll) {
-        case 1:
-            diceImg('dice-1.png');
-            break;
-        case 2:
-            diceImg('dice-2.png');
-            break;
-        case 3:
-            diceImg('dice-3.png');
-            break;
-        case 4:
-            diceImg('dice-4.png');
-            break;
-        case 5:
-            diceImg('dice-5.png');
-            break;
-        case 6:
-            diceImg('dice-6.png');
-            break;
-        default:
-            break;
-    };
-};
-
-// updateDiceImg(diceRoll);
-
-//Players
-const player1=document.querySelector('.player--0');
-
-let player1Score=0;
-let player1CurrScore=0;
-
-
-const player2=document.querySelector('.player--1');
-
-let player2Score=0;
-let player2CurrScore=0;
-
-//Function to execute when player becomes inactive
-function playerInactive(inactivePlayer,activePlayer,score,currScore,playerNumber,inactiveBool=true) {
-    let num=String(playerNumber);
-    if (inactiveBool) {
-        inactivePlayer.classList.remove('player--active');
-        activePlayer.classList.add('player--active');
+//Listen to roll dice button
+btnRoll.addEventListener('click', function () {
+//None of the code will execute if playing is false;
+  if (playing) {
+    //1. Generate Random Dice Roll
+    //This is not a global variable because each time you roll the die, if should generate a new number
+    const dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+    //2.Display a dice
+    //Show dice
+    diceEl.classList.remove('hidden');
+    //Use template literals to dynamically display dice image
+    diceEl.src = `dice-${dice}.png`;
+    //3.Check if rolled 1: true
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore += dice;
+      //Dynamically generating the current score for the current active player
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
     }
-    score+=currScore;
-    document.getElementById('score--'+num).textContent=score;
-    currScore=0;
-    document.getElementById('current--'+num).textContent=currScore;
-};
-
-function getPlayerStatus(player) {
-    player.classList.contains('player--active');
-}
-
-
-
-//Player 1 is the first to go, once they click the Roll Dice or Hold button, remove the class player--active from that player
-
-//If 1 is rolled, than next player goes
-
-
-//Listen to Button click on Roll Dice 
-
-//Starting with Player 1
-document.querySelector('.btn--roll').addEventListener('click',function() {
-
-    //Get player status
-    let player1Status=player1.classList.contains('player--active');
-    let player2Status=player2.classList.contains('player--active');
-    // let player1Status=getPlayerStatus(player1);
-    // let player2Status=getPlayerStatus(player2);
-
-
-    currDiceRoll=rollDice();
-    updateDiceImg(currDiceRoll);
-
-
-    if (player1Status===true) {
-        if (currDiceRoll>1) {
-            player1CurrScore+=currDiceRoll;
-            document.getElementById('current--0').textContent=player1CurrScore;
-        } else {
-            // player1.classList.remove('player--active');
-            // player2.classList.add('player--active');
-            // player1Score+=player1CurrScore;
-            // document.getElementById('score--0').textContent=player1Score;
-            // player1CurrScore=0;
-            // document.getElementById('current--0').textContent=player1CurrScore;
-            playerInactive(player1,player2,player1Score,player1CurrScore,0,true);
-        }
-    } else if (player2Status===true) {
-        if (currDiceRoll>1) {
-            player2CurrScore+=currDiceRoll;
-            document.getElementById('current--1').textContent=player2CurrScore;
-        } else {
-            // player1.classList.add('player--active');
-            // player2.classList.remove('player--active');
-            // player2Score+=player2CurrScore;
-            // document.getElementById('score--1').textContent=player2Score;
-            // player2CurrScore=0;
-            // document.getElementById('current--1').textContent=player2CurrScore;
-            playerInactive(player2,player1,player2Score,player2CurrScore,1,true);
-        }
-    } 
-
-
-
+  } 
 });
 
-//Listen to Button click on hold
-document.querySelector('.btn--hold').addEventListener('click',function() {
-    let player1Status=player1.classList.contains('player--active');
-    let player2Status=player2.classList.contains('player--active');
-    console.log(player2Status);
-    console.log(player1Status);
-    if (player1Status) {
-        playerInactive(player1,player2,player1Score,player1CurrScore,0,true);
-    } else if (player2Status) {
-        playerInactive(player2,player1,player2Score,player2CurrScore,1,true);
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. Add current score to the score of the active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //2. Check if score is at least 100
+    //Finish Game
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      //Add the winner class to the active player
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      //There's a competing class you want to remove so the winner class pulls through
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
     }
+  }
 });
 
 
-//Listen to New Game button click
-document.querySelector('.btn--new').addEventListener('click',function() {
-    // player1Score=0;
-    // player1CurrScore=0;
-    // document.getElementById('score--0').textContent=player1Score;
-    // document.getElementById('current--0').textContent=player1CurrScore;
-    // player2Score=0;
-    // player2CurrScore=0;
-
-    // document.getElementById('score--1').textContent=player2Score;
-    // document.getElementById('current--1').textContent=player2CurrScore;
-
-    currDiceRoll=5;
-    updateDiceImg(currDiceRoll);
-
-    playerInactive(player2,player1,0,0,1,true);
-    player1Score=0;
-    player1CurrScore=0;
-    playerInactive(player1,player2,0,0,0,false);
+btnNew.addEventListener('click', function () {
+  if (activePlayer===1) {
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+  };
+  startConditions();
 });
